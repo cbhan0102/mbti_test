9726362a3f19308c0b29ac03daded78007b6b633
cbhan0102 <cbhan0102@gmail.com>
2023-02-13 21:57:50 +0900

    [Add]test j

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-const el = document.querySelector('h1')
-console.log(el)
+const itemEls = document.querySelectorAll('.item')
+const btnEl = document.querySelector('.btn')
+const colors = ['red','orange','skyblue']
+
+btnEl.addEventListener('click', function() {
+    itemEls.forEach(function ( itemEl, index) {
+        console.log(index, itemEl)
+        itemEl.style.backgroundColor = colors[index]
+    })
+    btnEl.innerHTML = '<span>클릭 완료했습니다</span>'
+})
