7dcc04b48f30302865c7083948f11735f07acdb3
cbhan0102 <cbhan0102@gmail.com>
2023-02-16 22:33:06 +0900

    [Add]질문 js 만드는 중

---
+++ js/questions.js
@@ -0,0 +1,21 @@
+import { questions } from './data.js';
+
+const progressValueEl = document.querySelector('.progress .value')
+const numberEl = document.querySelector('.number')
+const questionEl = document.querySelector('.question')
+const choice1El = document.querySelector('.choice1')
+const choice2El = document.querySelector('.choice2')
+
+let currentNumber = 0
+let mbti = ''
+
+function renderQuestion() {
+    const question = questions[currentNumber]
+    numberEl.innerHTML = question.number
+    questionEl.innerHTML = question.question
+    choice1El.innerHTML = question.choices[0].text
+    choice2El.innerHTML = question.choices[1].text
+    progressValueEl.style.width = (currentNumber + 1) * 10 + '%'
+}
+
+renderQuestion()
