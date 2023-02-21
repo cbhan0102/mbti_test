5b4b412653c3423d3a51189590d0d8148cc4e490
cbhan0102 <cbhan0102@gmail.com>
2023-02-21 18:29:30 +0900

    [Add]결과페이지 뼈대

--- js/questions.js
+++ js/questions.js
@@ -19,6 +19,11 @@ function renderQuestion() { //변수들 할당
 }
 
 function nextQuestion(choiceNumber) {
+    if (currentNumber === questions.length - 1) { //현재 페이지가 0부터 시작해서 9라면.. 즉 10번째 마지막 페이지라면 결과페이지로 넘어간다.
+        showResultPage()
+        return
+    }
+
     const question = questions[currentNumber] //현재 질문에 대한 정보
     mbti = mbti + question.choices[choiceNumber].value 
     //기존에 mbti라는 변수가 가지고있는 값에 더하기~~ 초이스넘버 안에있는 값을 꺼내서 쓸 것임
@@ -26,6 +31,13 @@ function nextQuestion(choiceNumber) {
 
     renderQuestion() //질문 새롭게 렌더링
 }
+
+function showResultPage() {
+    location.href = '/results.html?mbti=' + mbti //쿼리스트링
+}
+
+
+
 
 choice1El.addEventListener('click', function() {
     nextQuestion(0) 
@@ -38,6 +50,3 @@ choice2El.addEventListener('click', function() {
 
 renderQuestion()
 
-function showResultPage() {
-    location.href = '/results.html?mbti=' + mbti //쿼리스트링
-}
